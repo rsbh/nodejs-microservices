7e4742b0acccaba81a140b412e9a789ca619b917
Rishabh Mishra <me@rsbh.dev>
2026-03-15 21:11:16 +0530

    feat: publish protobuf encoded events

--- services/test-kafka/publish-event.ts
+++ services/test-kafka/publish-event.ts
@@ -1,5 +1,10 @@
-import { Producer, ProduceAcks, stringSerializers } from "@platformatic/kafka";
+import { Producer, ProduceAcks, stringSerializer } from "@platformatic/kafka";
 import { v4 as uuid } from "uuid";
+import {
+  Product,
+  ProductEvent,
+  ProductEventType,
+} from "@rsbh-nodejs-microservices/protos/product/product";
 
 const BROKERS = (process.env.KAFKA_BROKERS || "localhost:9092").split(",");
 const SERVICE_NAME = process.env.SERVICE_NAME || "test-producer";
@@ -10,22 +15,36 @@ const TOPIC = "product.created";
 const producer = new Producer({
   clientId: `${SERVICE_NAME}-${HOSTNAME}`,
   bootstrapBrokers: BROKERS,
-  serializers: stringSerializers,
+  serializers: {
+    key: stringSerializer,
+    value: (value?: ProductEvent) => {
+      const event = value ?? ProductEvent.create();
+      return Buffer.from(ProductEvent.encode(event).finish());
+    },
+  },
   acks: ProduceAcks.ALL,
 });
 
 async function main() {
-  const product = {
+  const product = Product.create({
+    id: Math.random() * 1000,
+    name: "Test Product",
+  });
+
+  const event = ProductEvent.create({
     id: uuid(),
-    name: "Test",
-  };
+    type: ProductEventType.PRODUCT_EVENT_TYPE_CREATED,
+    product: product,
+    timestamp: new Date(),
+  });
+
   try {
     const { offsets } = await producer.send({
       messages: [
         {
           topic: TOPIC,
-          key: product.id,
-          value: JSON.stringify(product),
+          key: event.id,
+          value: event,
         },
       ],
     });
